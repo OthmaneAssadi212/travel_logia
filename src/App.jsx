53fa2765a3e4afeec503f8ed2833895c11c036a4
otothiis <othmane.gb00@gmail.com>
2025-07-03 17:30:42 +0100

    second one

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import colors from './constants/colors';
+
+function App() {
+  return (
+ <div
+      className="min-h-screen flex items-center justify-center"
+      style={{ backgroundColor: colors.ivory }}
+    >
+      <p
+        className="text-xl font-semibold shadow-lg p-4 rounded"
+        style={{
+          color: colors.eucalyptus,
+          backgroundColor: colors.olive,
+        }}
+      >
+        Hello world
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/colors.jsx
@@ -0,0 +1,8 @@
+const colors = {
+  eucalyptus: '#819A91',  
+  mint: '#A7C1A8',        
+  olive: '#D1D8BE',        
+  ivory: '#EEEFE0',        
+};
+
+export default colors;
